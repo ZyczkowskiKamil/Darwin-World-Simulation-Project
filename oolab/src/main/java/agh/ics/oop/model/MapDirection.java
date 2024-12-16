a0f71e9882af1ee318db916820e87ec1fa9fbcc2
ZyczkowskiKamil <zyczkowskik04@gmail.com>
2024-12-16 19:23:04 +0100

    Add MapDirection

---
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -0,0 +1,54 @@
+package agh.ics.oop.model;
+
+public enum MapDirection {
+    NORTH,
+    NORTH_EAST,
+    EAST,
+    SOUTH_EAST,
+    SOUTH,
+    SOUTH_WEST,
+    WEST,
+    NORTH_WEST;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case NORTH -> "north";
+            case NORTH_EAST -> "north-east";
+            case EAST -> "east";
+            case SOUTH_EAST -> "south-east";
+            case SOUTH -> "south";
+            case SOUTH_WEST -> "south-west";
+            case WEST -> "west";
+            case NORTH_WEST -> "north-west";
+        };
+    }
+
+    public static MapDirection valueToMapDirection(int value) {
+        return switch (value) {
+            case 0 -> NORTH;
+            case 1 -> NORTH_EAST;
+            case 2 -> EAST;
+            case 3 -> SOUTH_EAST;
+            case 4 -> SOUTH;
+            case 5 -> SOUTH_WEST;
+            case 6 -> WEST;
+            case 7 -> NORTH_WEST;
+
+            default -> throw new IllegalArgumentException("Value needs to be integer from 0 to 7: " + value);
+        };
+    }
+
+    public Vector2d toUnitVector() {
+        return switch (this) {
+            case NORTH -> new Vector2d(0, 1);
+            case NORTH_EAST -> new Vector2d(1, 1);
+            case EAST -> new Vector2d(1,0);
+            case SOUTH_EAST -> new Vector2d(1, -1);
+            case SOUTH -> new Vector2d(0, -1);
+            case SOUTH_WEST -> new Vector2d(-1, -1);
+            case WEST -> new Vector2d(-1, 0);
+            case NORTH_WEST -> new Vector2d(-1,1);
+        };
+    }
+}
