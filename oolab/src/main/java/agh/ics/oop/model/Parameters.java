342f22e87546f9679169e8eaa012d92f28b88704
ZyczkowskiKamil <zyczkowskik04@gmail.com>
2025-01-07 19:12:11 +0100

    Parameters file reading changed

--- oolab/src/main/java/agh/ics/oop/model/Parameters.java
+++ oolab/src/main/java/agh/ics/oop/model/Parameters.java
@@ -1,6 +1,7 @@
 package agh.ics.oop.model;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class Parameters {
@@ -25,24 +26,69 @@ public class Parameters {
     public Parameters() throws FileNotFoundException {
 
         try {
-            String filePath = System.getProperty("user.dir") + "\\src\\main\\java\\agh\\ics\\oop\\model\\" + "parameters.txt";
-            Scanner scanner = new Scanner(new File(filePath));
+            URL parametersURL = getClass().getClassLoader().getResource("parameters/parameters.txt");
+            Scanner scanner = new Scanner(new File(parametersURL.getFile()));
 
-            if (scanner.hasNextLine()) this.MAP_HEIGHT = scanner.nextInt();
-            if (scanner.hasNextLine()) this.MAP_WIDTH = scanner.nextInt();
-            if (scanner.hasNextLine()) this.WATER_MAP_VARIANT = scanner.nextBoolean();
-            if (scanner.hasNextLine()) this.GRASS_START_AMOUNT = scanner.nextInt();
-            if (scanner.hasNextLine()) this.ENERGY_ADDED_AFTER_EATING_GRASS = scanner.nextInt();
-            if (scanner.hasNextLine()) this.NUMBER_OF_GRASS_GROWING_DAILY = scanner.nextInt();
-            if (scanner.hasNextLine()) this.START_ANIMAL_NUMBER = scanner.nextInt();
-            if (scanner.hasNextLine()) this.START_ANIMAL_ENERGY = scanner.nextInt();
-            if (scanner.hasNextLine()) this.BREEDING_READY_ANIMAL_ENERGY = scanner.nextInt();
-            if (scanner.hasNextLine()) this.ENERGY_LOST_IN_REPRODUCTION = scanner.nextInt();
-            if (scanner.hasNextLine()) this.MIN_MUTATION_NUMBER = scanner.nextInt();
-            if (scanner.hasNextLine()) this.MAX_MUTATION_NUMBER = scanner.nextInt();
-            if (scanner.hasNextLine()) this.GENES_LENGTH = scanner.nextInt();
-            if (scanner.hasNextLine()) this.AGING_ANIMALS = scanner.nextBoolean();
-            if (scanner.hasNextLine()) this.ENERGY_NEEDED_FOR_MOVEMENT = scanner.nextInt();
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.MAP_HEIGHT = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.MAP_WIDTH = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.WATER_MAP_VARIANT = (tmp.equals("true"));
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.GRASS_START_AMOUNT = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.ENERGY_ADDED_AFTER_EATING_GRASS = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.NUMBER_OF_GRASS_GROWING_DAILY = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.START_ANIMAL_NUMBER = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.START_ANIMAL_ENERGY = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.BREEDING_READY_ANIMAL_ENERGY = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.ENERGY_LOST_IN_REPRODUCTION = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.MIN_MUTATION_NUMBER = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.MAX_MUTATION_NUMBER = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.GENES_LENGTH = Integer.parseInt(tmp);
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.AGING_ANIMALS = (tmp.equals("true"));
+            }
+            if (scanner.hasNextLine()) {
+                String tmp = scanner.nextLine().split("=")[1];
+                this.ENERGY_NEEDED_FOR_MOVEMENT = Integer.parseInt(tmp);
+            }
             scanner.close();
         }
         catch (FileNotFoundException e) {
