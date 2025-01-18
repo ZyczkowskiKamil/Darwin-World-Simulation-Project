a07ea973f189bcf50a97b2b4b0dffe4e1186e9b0
ZyczkowskiKamil <zyczkowskik04@gmail.com>
2025-01-18 12:54:00 +0100

    Aplication windows shows

--- oolab/src/main/java/agh/ics/oop/SimulationApp.java
+++ oolab/src/main/java/agh/ics/oop/SimulationApp.java
@@ -13,34 +13,9 @@ import java.io.FileNotFoundException;
 
 public class SimulationApp extends Application {
 
-    static agh.ics.oop.model.Parameters parameters;
-
-    static {
-        try {
-            parameters = new agh.ics.oop.model.Parameters();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    GlobeMap worldMap = new GlobeMap(parameters);
-
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
-        BorderPane viewRoot = loader.load();
-        SimulationPresenter presenter = loader.getController();
-
-
-
-        presenter.setWorldMap(worldMap);
-
-        Simulation simulation = new Simulation(presenter, worldMap);
-        simulation.run();
-
-        configureStage(primaryStage, viewRoot);
         primaryStage.show();
     }
 
@@ -51,4 +26,43 @@ public class SimulationApp extends Application {
         primaryStage.minWidthProperty().bind(viewRoot.minWidthProperty());
         primaryStage.minHeightProperty().bind(viewRoot.minHeightProperty());
     }
+
+//    static agh.ics.oop.model.Parameters parameters;
+//
+//    static {
+//        try {
+//            parameters = new agh.ics.oop.model.Parameters();
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    GlobeMap worldMap = new GlobeMap(parameters);
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+////
+////        FXMLLoader loader = new FXMLLoader();
+////        loader.setLocation(getClass().getClassLoader().getResource("simulation.fxml"));
+////        BorderPane viewRoot = loader.load();
+////        SimulationPresenter presenter = loader.getController();
+////
+////
+////
+////        presenter.setWorldMap(worldMap);
+////
+////        Simulation simulation = new Simulation(presenter, worldMap);
+////        simulation.run();
+////
+////        configureStage(primaryStage, viewRoot);
+////        primaryStage.show();
+//    }
+//
+//    private void configureStage(Stage primaryStage, BorderPane viewRoot) {
+//        var scene = new Scene(viewRoot);
+//        primaryStage.setScene(scene);
+//        primaryStage.setTitle("Simulation app");
+//        primaryStage.minWidthProperty().bind(viewRoot.minWidthProperty());
+//        primaryStage.minHeightProperty().bind(viewRoot.minHeightProperty());
+//    }
 }
